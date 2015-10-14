861bfdabc5af5b97bc48c1e9ee87b93eeb21a6af
mi546870961 <chenyushen1989@163.com>
2015-10-14 10:26:49 +0800

    百度统计功能添加
    百度统计功能添加

---
+++ js/baidutongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?98a9629da48d46e4910661030624f047";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
